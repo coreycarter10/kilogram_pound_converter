40a1995f4876ca0e4c80cab5bb30b31a5399b712
Corey Carter <corey-carter@hotmail.com>
2020-03-25 09:35:16 -0600

    Updated orientation preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 double kgToPounds(double kg) => kg * 2.205;
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
